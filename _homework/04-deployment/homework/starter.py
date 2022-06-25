04971484e26a26db218e25a51126e59a2f174af8
Dese Narfa Firmansyah <dese.firmansyah@protonmail.com>
2022-06-25 21:36:42 +0700

    feat(homework-4): add q5 and q6 answers

--- _homework/04-deployment/homework/starter.py
+++ _homework/04-deployment/homework/starter.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-get_ipython().system('pip freeze | grep scikit-learn')
-
-
-# change into scikit 1.0.2
-
-get_ipython().system('pip install scikit-learn==1.0.2')
-
-
 import pickle
+import sys
 import pandas as pd
-
-
-with open('model.bin', 'rb') as f_in:
-    dv, lr = pickle.load(f_in)
 
 
 categorical = ['PUlocationID', 'DOlocationID']
@@ -31,45 +19,23 @@ def read_data(filename):
     
     return df
 
-
-df = read_data('https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2021-02.parquet')
-
-
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = lr.predict(X_val)
+def run():
+    year = int(sys.argv[1]) # 2021
+    month = int(sys.argv[2]) # 3
+    with open('model.bin', 'rb') as f_in:
+        dv, lr = pickle.load(f_in)
 
 
-# ## Question 1
-
-print(y_pred.mean())
+    df = read_data(f'https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_{year:04d}-{month:02d}.parquet')
 
 
-# ## Question 2
-
-year = 2022
-month = 2
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
-df
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = lr.predict(X_val)
 
 
-df_result = df[['ride_id']].copy()
-df_result['prediction'] = y_pred
-df_result
+    print(y_pred.mean())
 
 
-df_result.to_parquet(
-    'fhv_tripdata_2022-02_ride_id_predictions.parquet',
-    engine='pyarrow',
-    compression=None,
-    index=False
-)
-
-
-get_ipython().system('ls -lh')
-
-
-# ## Question 3
-
-get_ipython().system('jupyter nbconvert --to script starter.ipynb')
-
+if __name__ == '__main__':
+    run()
